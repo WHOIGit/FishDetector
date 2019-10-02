b801506a3f6f614bb7f9e71a18f70ad07250de97
Ryan Govostes <rgovostes@whoi.edu>
2019-10-02 12:30:30 -0400

    Add ability to select true negative images

--- generate_train_list.py
+++ generate_train_list.py
@@ -6,6 +6,7 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dir', required=True)
+parser.add_argument('--negatives')
 parser.add_argument('--train-ratio', default=0.9)
 args = parser.parse_args()
 
@@ -13,6 +14,21 @@ args = parser.parse_args()
 listing = [ os.path.join(args.dir, f) for f in os.listdir(args.dir) ]
 files = [ f for f in listing \
         if f.endswith('.jpg') and f.replace('.jpg', '.txt') in listing ]
+
+# Add a random set of true negatives
+if args.negatives:
+    negatives = [ os.path.join(args.negatives, f)
+                  for f in os.listdir(args.negatives) if f.endswith('.jpg') ]
+
+    # Pick a random set of true negatives to add to the files list
+    negatives = random.sample(negatives, len(files))
+    files.extend(negatives)
+
+    # Create empty bounding box files
+    for f in negatives:
+        if not f.replace('.jpg', '.txt') in listing:
+            open(f.replace('.jpg', '.txt'), 'w').close()
+
 
 random.shuffle(files)
 ntrain = int(args.train_ratio * len(files))
