4299e3f9d645f4f4a78243b6a1cb16e543fbdb6b
Ryan Govostes <rgovostes@whoi.edu>
2020-04-29 11:28:41 -0400

    Ditch the JSON idea and just special case options we want

--- configtool.py
+++ configtool.py
@@ -1,50 +1,88 @@
 import argparse
-import json
+import collections
 import re
 
 
+Section = collections.namedtuple('Section', 'index name attributes')
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--to-json', action='store_true')
-parser.add_argument('--to-cfg', dest='to_json', action='store_false')
+parser.add_argument('--batch', type=int)
+parser.add_argument('--classes', type=int)
+parser.add_argument('--no-color-adjustments', action='store_true')
+parser.add_argument('--size', nargs=2, type=int)
+parser.add_argument('--subdivisions', type=int)
 parser.add_argument('baseconfig')
 args = parser.parse_args()
-
-if args.baseconfig == '-':
-    args.baseconfig = '/dev/stdin'
+print(args)
 
 
-if args.to_json:
-    # Parse the base configuration
-    config = []
-    with open(args.baseconfig) as f:
-        for line in f:
-            m = re.match(r'\[(.*?)\]', line)
-            if m:
-                config.append({
-                    'section': m.group(1),
-                    'attributes': {},
-                })
-            
-            m = re.match(r'\s*(\w+)\s*=\s*(.*?)\s*$', line)
-            if m:
-                key, value = m.group(1), m.group(2)
-                config[-1]['attributes'][key] = value
+# Parse the base configuration
+config = []
+with open(args.baseconfig) as f:
+    for line in f:
+        m = re.match(r'\[(.*?)\]', line)
+        if m:
+            config.append(Section(
+                index=len(config),
+                name=m.group(1),
+                attributes={},
+            ))
+        
+        m = re.match(r'\s*(\w+)\s*=\s*(.*?)\s*$', line)
+        if m:
+            key, value = m.group(1), m.group(2)
+            config[-1].attributes[key] = value
 
-    # Add previous and next section hints
-    for i, group in enumerate(config):
-        group['prev'] = config[i]['section'] if i > 0 else None
-        group['next'] = config[i+1]['section'] if i < len(config) - 1 else None
 
-    print(json.dumps(config))
+def getsection(name):
+    return next(getsections(name))
 
-else:
-    with open(args.baseconfig) as f:
-        config = json.load(f)
+def getsections(name):
+    return filter(lambda x: x.name == name, config)
+
+
+# Make adjustments to configuration
+net = getsection('net')
+
+if args.batch is not None:
+    net.attributes['batch'] = args.batch
+
+if args.classes is not None:
+    net.attributes['max_batches'] = max_batches = \
+        max(4000, args.classes * 2000)
+    net.attributes['steps'] = \
+        f'{int(0.8*max_batches)},{int(0.9*max_batches)}'
     
-    # Serialize out the new configuration
-    for i, obj in enumerate(config):
-        if i > 0:
-            print()
-        print(f'[{obj["section"]}]')
-        for key, value in obj['attributes'].items():
-            print(f'{key} = {value}')
+    for section in getsections('yolo'):
+        section.attributes['classes'] = args.classes
+        config[section.index-1].attributes['filters'] = (args.classes + 5) * 3
+    
+    for section in getsections('gaussian_yolo'):
+        section.attributes['classes'] = args.classes
+        config[section.index-1].attributes['filters'] = (args.classes + 9) * 3
+
+if args.no_color_adjustments == True:
+    for attr in ('hue', 'saturation', 'exposure'):
+        try:
+            del net.attributes[attr]
+        except KeyError:
+            pass
+
+if args.size is not None:
+    assert args.size[0] % 32 == 0
+    assert args.size[1] % 32 == 0
+    net.attributes['width'] = args.size[0]
+    net.attributes['height'] = args.size[1]
+
+if args.subdivisions is not None:
+    net.attributes['subdivisions'] = args.subdivisions
+
+
+# Serialize out the new configuration
+for section in config:
+    if section.index > 0:
+        print()
+    print(f'[{section.name}]')
+    for key, value in section.attributes.items():
+        print(f'{key} = {value}')
